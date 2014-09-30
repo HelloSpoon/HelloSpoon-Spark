680baaea177c0a4a52f2c68610e6bee24a1542f8
HelloSpoon embedded Dev <hellospoonpr@gmail.com>
2014-09-29 21:25:38 -0600

    Create sample.ino

---
+++ sample.ino
@@ -0,0 +1,24 @@
+/*
+Code to test XL-320 with Arduino.
+
+1. Initialize the comm.
+2. Move XL-320 actuator.
+3. Turn on LED of XL-320.
+
+To know the full list of instructions check inside XL320.h
+
+Made by Luis G III for HelloSpoon robot.
+
+*/
+
+#include <XL320.h>
+
+void setup(){
+  XL320.begin();
+  delay(1000);
+  XL320.writeWord(5,30,512);
+  delay(1000);
+  XL320.writeWord(5,25,1);
+  delay(1000);
+}
+void loop(){}
