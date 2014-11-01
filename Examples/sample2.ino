4b45e148d6d050b9f7565f77d7af8f6cb4ba88d5
HelloSpoon embedded Dev <hellospoonpr@gmail.com>
2014-11-01 01:19:53 -0700

    Update sample2.ino

--- Examples/sample2.ino
+++ Examples/sample2.ino
@@ -5,17 +5,17 @@ void setup(){
   hs.begin();
   delay(1000);
 
-  hs.LED(5, 4);
-  hs.setJointSpeed(5, 1023);
-  hs.moveJoint(5, 212);
+  hs.LED(4, 4);
+  hs.setJointSpeed(4, 1023);
+  hs.moveJoint(4, 212);
   delay(1000);
 
 }
 
 void loop(){  
 
-  hs.moveJoint(5,random(212,512));
-  hs.LED(5,random(1,7));
+  hs.moveJoint(4,random(212,512));
+  hs.LED(4,random(1,7));
   delay(1000);  
 
 }
