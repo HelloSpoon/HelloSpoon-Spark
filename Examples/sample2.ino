7b4b8ae8e15114758a6a22a296c86bd023b1cbb2
HelloSpoon embedded Dev <hellospoonpr@gmail.com>
2014-11-01 01:40:02 -0700

    Update sample2.ino

--- Examples/sample2.ino
+++ Examples/sample2.ino
@@ -2,8 +2,8 @@
 Code to test HelloSpoon behavior.
 
   1. Initialize the comm.
-  2. Turn Dynamixel ID 4 LED ON, Yellow color.
-  3. Move Dynamixel ID 4.
+  2. Turn Dynamixel ID 5 LED ON, Yellow color.
+  3. Move Joint #4.
   4. Random positions and LED colors are generated in Loop.
 
 To learn the full list of methods check inside HelloSpoon.h
