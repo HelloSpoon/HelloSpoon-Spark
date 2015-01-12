d391b93e3f966fb9a03b948c2d762a26362abc34
HelloSpoon embedded Dev <hellospoonpr@gmail.com>
2015-01-11 20:37:33 -0700

    Create Example_1.ino

---
+++ firmware/examples/Example_1.ino
@@ -0,0 +1,29 @@
+/*
+Code to test HelloSpoon #1.
+
+1. Initialize the comm.
+2. Move XL-320 actuator.
+3. Turn on LED of XL-320.
+
+To know the full list of instructions check inside HelloSpoon.h
+
+To learn how to upload this code and custom codes to HelloSpoon
+please visit the Wiki included in this repository.
+
+Made by Luis G III for HelloSpoon robot.
+
+*/
+
+#include "HelloSpoon.h"
+
+void setup(){
+  hs.begin();
+  delay(1000);
+  
+  hs.writeWord(4,30,512);
+  delay(1000);
+  
+  hs.writeWord(4,25,1);
+  delay(1000);
+}
+void loop(){}
