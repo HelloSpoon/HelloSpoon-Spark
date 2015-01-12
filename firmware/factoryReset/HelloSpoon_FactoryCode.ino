953a8b0762cadd00e6f7b3ebe01635eaf4923b3c
HelloSpoon embedded Dev <hellospoonpr@gmail.com>
2015-01-11 20:38:34 -0700

    Create HelloSpoon_FactoryCode.ino

---
+++ firmware/factoryReset/HelloSpoon_FactoryCode.ino
@@ -0,0 +1,17 @@
+/*
+This is the original code provided with your HelloSpoon.
+
+All the Mobile Suite from HelloSpoon will work 
+while this code is loaded in your robot.
+If you load a new code directly from Spark IDE (http://spark.io/build)
+all the Mobile Suite (Animator, Controller and HelloSpoon App) will stop working.
+
+To learn how to upload this code and custom codes to HelloSpoon
+please visit the Wiki included in this repository.
+
+Hope this helps you!
+Happy coding :-)!
+
+- QOLbotics Team.
+
+*/
