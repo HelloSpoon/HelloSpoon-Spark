c64eecb4f364a1b0c1460d07c11f49fd580cdfde
HelloSpoon embedded Dev <hellospoonpr@gmail.com>
2015-01-22 02:32:16 -0700

    Update HelloSpoon_FactoryCode.ino

--- firmware/factoryReset/HelloSpoon_FactoryCode.ino
+++ firmware/factoryReset/HelloSpoon_FactoryCode.ino
@@ -15,3 +15,68 @@ Happy coding :-)!
 - QOLbotics Team.
 
 */
+
+#include "HelloSpoon-Spark/HelloSpoon-Spark.h"
+#include "Serial2/Serial2.h"
+
+SYSTEM_MODE(SEMI_AUTOMATIC); //This runs setup() immidiately and you have to manually connect to the Spark Cloud.
+
+HelloSpoon robot;
+
+void setup() {
+    
+    //EEPROM.write(1, 0); //Mandatory use the first time you load the code, then please comment it.
+    
+    if(EEPROM.read(1) == 0){
+        HelloSpoon_FirstUsage();    
+    }
+    else{
+        HelloSpoon_setup();
+    }
+    
+}
+
+void loop() {
+    
+    if(Serial2.available()){
+        if(Serial2.read()=='w'){
+            HelloSpoon_Online();
+        }
+    }
+    
+}
+
+void HelloSpoon_setup(){
+    /*Start HelloSpoon robot*/
+    robot.begin();
+    /*Start Bluetooth communication*/
+    Serial2.begin(57600);
+    /*Turn off Wifi module to save battery*/
+    WiFi.off();
+    delay(1000);
+    /*Activate HelloSpoon's trunk*/
+    robot.activateTrunk();
+}
+
+void HelloSpoon_Online(){
+    /*Turn on WiFi module*/
+    WiFi.on();
+    delay(1000);
+    /*Connect to the already known Wifi network*/
+    WiFi.connect();
+    delay(1000);
+    /*Connect to Spark Cloud*/
+    Spark.connect();
+}
+
+void HelloSpoon_FirstUsage(){
+    EEPROM.write(1, 0x01); //Change value on EEPROM's second byte.
+    /*Turn on WiFi module*/
+    WiFi.on();
+    delay(1000);
+    /*Connect to the already known Wifi network*/
+    WiFi.connect();
+    delay(1000);
+    /*Connect to Spark Cloud*/
+    Spark.connect();
+}
